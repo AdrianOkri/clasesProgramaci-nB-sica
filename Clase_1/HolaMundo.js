481d85630f3d5151ac97fded6b713af50c6c3418
Adrián Okri <noe.ricoy@hotmail.com>
2018-04-13 23:41:29 +0200

    first commit

---
+++ Clase_1/HolaMundo.js
@@ -0,0 +1,29 @@
+// **************** Clase 1 ****************
+
+/*
+    Vas a escribir tu primir línea de código, y aprender a programar.
+    ve a tu navegador web y en la caja de texto de las URL´s escribir about:blank
+    esto simplemente nos dará una página en blanco.
+
+    Abrir el inspector, ahí nos dirá como está construida una página web.
+    de momento eso no nos importa. ve a la parte donde dice console.
+    ahí podrás colocar la siguiente instrucción
+*/
+alert ("Hola, mi nombre es Okri y estoy dando clase"); // <-- Tu primir instrucción en código !!
+
+//Hagamos algo un poco más díficil, guardar un valor en un variable
+
+var nombre = "Okri";
+// De momento no ha pasado nada, pero si escribes nombre en la consola, podrás ver que te regresará Okri
+alert ("Mi nombre es " + nombre); // ¿Qué crees que pase en esto?
+
+nombre = "Juana la Alpaca"; // Ahora nombre es igual a Juana la Alpaca, ya no es Okri
+alert ("Mi nombre es " + nombre); // ¿Qué crees que pase en esto?
+
+// Hay que subir el nivel, solamente confiar en mí
+nombre = prompt("¿Cuál es tu nombre?"); // Ahora el programa te pide que le ingreses el nombre
+alert ("Mi nombre es " + nombre); // ¿Qué crees que pase en esto?
+
+// Bienvenido al mundo de la programación, donde puedes cambiar el mundo por completo
+// y todo parte desde que tu entiendes que si tengo una variable x = 1; y una varible y = 2;
+// al hacer x + y, me va a dar 3.
